ec4c64e222be240540cab7926b624ab1410459b5
Carla Garcia <carlagrsmm@gmail.com>
2026-04-23 19:06:59 +0200

    Código para 4 sensores

--- src/tocadiscos/tocadiscos.ino
+++ src/tocadiscos/tocadiscos.ino
@@ -1,6 +1,6 @@
 #include <DFRobotDFPlayerMini.h>
 
-#define ALTURA_MAXIMA 10
+#define ALTURA_MAXIMA 15
 // Rango de lectura de la ficha baja
 #define BAJA_MIN 3
 #define BAJA_MAX 4.5
@@ -8,48 +8,58 @@
 #define ALTA_MIN 6
 #define ALTA_MAX 7.5
 
-//0 para vacío, 1 para ficha baja, 2 para ficha alta
-int estadoSensor[4] = {0, 0, 0, 0};
+const int Trig[] = {2, 4, 6, A0}; 
+const int Echo[] = {3, 5, 7, A1}; 
 
-const int Trig = 9; 
-const int Echo = 8; 
+//0 para vacío, 1 para ficha baja, 2 para ficha alta
+int estadoSensor[] = {0, 0, 0, 0};
+
 
 void setup() {
   Serial.begin(115200);
-  pinMode(Trig, OUTPUT);
-  pinMode(Echo, INPUT);
+  for (int i = 0; i < 4; i++) {
+    pinMode(Trig[i], OUTPUT);
+    pinMode(Echo[i], INPUT);
+  }
   Serial.println("--- ESPERANDO FICHAS ---");
 }
 
-int medir(int i){
-  digitalWrite(Trig, LOW);
-  delayMicroseconds(5);
-  digitalWrite(Trig, HIGH);
+int medirDistancia(int i){
+  digitalWrite(Trig[i], LOW);
+  delayMicroseconds(2);
+  digitalWrite(Trig[i], HIGH);
   delayMicroseconds(10);
-  digitalWrite(Trig, LOW);
+  digitalWrite(Trig[i], LOW);
 
-  long tiempo = pulseIn(Echo, HIGH, 20000);
+  long tiempo = pulseIn(Echo[i], HIGH, 20000);
   if (tiempo == 0) return 999;
   return tiempo * 0.034 / 2;
 }
 
 void loop() {
-  int distancia = medir(0);
-  if (distancia >= BAJA_MIN && distancia <= BAJA_MAX){
-    if (estadoSensor[0] != 1){
-      Serial.println("Nota 1: DO");
-      estadoSensor[0] = 1;
-    }
-  }else if (distancia >= ALTA_MIN && distancia <= ALTA_MAX){
-      if (estadoSensor[0] != 2){
-        Serial.println("Nota 2: RE");
-        estadoSensor[0] = 2;
+  for(int i = 0; i < 4; i++){
+    int distancia = medirDistancia(i);
+    
+    if (distancia >= BAJA_MIN && distancia <= BAJA_MAX){
+      if (estadoSensor[i] != 1){
+        int notaATocar = (i * 2) + 1;
+        Serial.print("Sensor "); Serial.print(i+1);
+        Serial.print(" - Nota detectada: "); Serial.println(notaATocar);
+        estadoSensor[i] = 1;
       }
-  }else if (distancia > ALTURA_MAXIMA) {
-    if (estadoSensor[0] != 0) {
-      Serial.println("Estado: Vacío");
-      estadoSensor[0] = 0;
+    }else if (distancia >= ALTA_MIN && distancia <= ALTA_MAX){
+      if (estadoSensor[i] != 2){
+        int notaATocar = (i * 2) + 2;
+        Serial.print("Sensor "); Serial.print(i+1);
+        Serial.print(" - Nota detectada: "); Serial.println(notaATocar);
+        estadoSensor[i] = 2;
+      }
+    }else if (distancia > ALTURA_MAXIMA) {
+      if (estadoSensor[i] != 0) {
+        Serial.println("Estado: Vacío");
+        estadoSensor[i] = 0;
+      }
     }
   }
-  delay(100);
+  delay(30);
 }
